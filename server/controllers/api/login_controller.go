94bbd5fff6006ff85e7da24dade202c1def4087c
erlangs <helinbo@qq.com>
2022-03-15 20:22:31 +0800

    add signin2

--- server/controllers/api/login_controller.go
+++ server/controllers/api/login_controller.go
@@ -42,6 +42,29 @@ func (c *LoginController) PostSignup() *simple.JsonResult {
 }
 
 // 用户名密码登录
+func (c *LoginController) PostSignin2() *simple.JsonResult {
+	var (
+		//captchaId   = c.Ctx.PostValueTrim("captchaId")
+		//captchaCode = c.Ctx.PostValueTrim("captchaCode")
+		username = c.Ctx.PostValueTrim("username")
+		password = c.Ctx.PostValueTrim("password")
+		ref      = c.Ctx.FormValue("ref")
+	)
+	// loginMethod := services.SysConfigService.GetLoginMethod()
+	// if !loginMethod.Password {
+	// 	return simple.JsonErrorMsg("账号密码登录/注册已禁用")
+	// }
+	//if !captcha.VerifyString(captchaId, captchaCode) {
+	//	return simple.JsonError(common.CaptchaError)
+	//}
+	user, err := services.UserService.SignIn(username, password)
+	if err != nil {
+		return simple.JsonErrorMsg(err.Error())
+	}
+	return render.BuildLoginSuccess(user, ref)
+}
+
+// 用户名密码登录
 func (c *LoginController) PostSignin() *simple.JsonResult {
 	var (
 		captchaId   = c.Ctx.PostValueTrim("captchaId")
